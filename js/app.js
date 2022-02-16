231fae6a26622d6c6f1900e764db8b4aa032e3b4
Marzan-Bin-Rahman <mail.to.marzan@gmail.com>
2022-02-17 02:06:28 +0600

    first function done

--- js/app.js
+++ js/app.js
@@ -1,42 +1,61 @@
+// creating a function for getting the inputs
+
+function getInputValue(inputId){
+    const inputValue = document.getElementById(inputId);
+    const inputValueText = inputValue.value;
+    const inputValueAmount = parseFloat(inputValueText);
+    
+    // clearing the value
+    inputValue.value = "";
+    return inputValueAmount;
+}
+
+
 // event listener to calculate button 
 
 document.getElementById('calculate-button').addEventListener('click', function(){
     // getting income input
-    const incomeInput = document.getElementById('income-input');
+
+    const incomeInputAmount = getInputValue('income-input');
+
+    /* const incomeInput = document.getElementById('income-input');
     const incomeInputText = incomeInput.value;
     const incomeInputAmount = parseFloat(incomeInputText);
     
     // clearing the value
-    incomeInput.value = "";
+    incomeInput.value = ""; */
 
     // getting expenses
 
     // getting food amount
+    const foodInputAmount = getInputValue('food-input');
 
-    const foodInput = document.getElementById('food-input');
+    /* const foodInput = document.getElementById('food-input');
     const foodInputText = foodInput.value;
     const foodInputAmount = parseFloat(foodInputText);
 
     // clearing the value
-    foodInput.value = "";
+    foodInput.value = ""; */
 
     // getting rent amount
+    const rentInputAmount = getInputValue('rent-input');
 
-    const rentInput = document.getElementById('rent-input');
+    /* const rentInput = document.getElementById('rent-input');
     const rentInputText = rentInput.value;
     const rentInputAmount = parseFloat(rentInputText);
     
     // clearing the value
-    rentInput.value = "";
+    rentInput.value = ""; */
 
     // getting clothes amount
+    const clothInputAmount = getInputValue('cloth-input');
 
-    const clothInput = document.getElementById('cloth-input');
+    /* const clothInput = document.getElementById('cloth-input');
     const clothInputText = clothInput.value;
     const clothInputAmount = parseFloat(clothInputText);
     
     // clearing the value
-    clothInput.value = "";
+    clothInput.value = ""; */
 
     // setting total expenses and balance
 
